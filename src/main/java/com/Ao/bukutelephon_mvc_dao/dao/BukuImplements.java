cba07e57f53ae0b7694f99a1d92db3bcd81ed39e
JustAo <51353623+Love2Bit@users.noreply.github.com>
2022-02-18 11:09:06 +0700

    first Commit

---
+++ src/main/java/com/Ao/bukutelephon_mvc_dao/dao/BukuImplements.java
@@ -0,0 +1,22 @@
+package com.Ao.bukutelephon_mvc_dao.dao;
+
+import com.Ao.bukutelephon_mvc_dao.model.BukuTelephon;
+
+import java.util.List;
+
+public interface BukuImplements {
+
+    public void insert(BukuTelephon b);
+
+    public void update(BukuTelephon b);
+
+    public void delete(BukuTelephon id);
+
+    public BukuTelephon getId(int id);
+
+    public List<BukuTelephon> getALL();
+
+    public void getCariNama(String nama);
+
+    List<BukuTelephon> search(BukuTelephon buku);
+}
